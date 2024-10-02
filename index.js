2e6ba9896dd36ada3e1d75c48858c5de4a940cb1
Matias <matias.karkia@gmail.com>
2024-10-02 17:55:46 +0300

    alku

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
-const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
